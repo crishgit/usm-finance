5ca9ea3f8f0bb4eb67611d002ef4a09382e466b9
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-06-03 13:05:40 -0400

    add finance-software.cpp with the semi-finished Debt class

---
+++ finance-software.cpp
@@ -0,0 +1,72 @@
+#include <math.h>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+// add extraordinary ammounts to a given time of the month
+// the extraordinary ammounts are (ammount, month) and substract them from the debt
+// cláusula cuando en un mes, el monto extraodinario + pago mensual es mayor a la deuda restante
+// falta: total interes pagados
+class Debt {
+private:
+    int initial_debt;
+    int ammount;
+    int interest;
+    int time;
+    int payment;
+    // actual refered to the interes/debt paid for the current month ->
+    int actual_interest_paid;
+    int actual_debt_paid;
+    int time_passed;
+    int ammount_paid;
+    // vector <int> extraordinary_ammounts;
+
+public:
+    Debt (int ammount, int interest, int time){
+        this->initial_debt= ammount;
+        this->ammount = ammount;
+        this->interest = interest;
+        this->time = time;
+        this->payment = (ammount * interest) / (1 - pow(1 + interest, -time));
+        this->actual_interest_paid = 0;
+        this->actual_debt_paid = 0;
+        this->time_passed = 0;
+        this->ammount_paid = 0;
+    }
+
+    void pass_month() {
+        if(this->time_passed > this->time){
+            return;
+        }
+
+        this->time_passed++;
+        this->actual_interest_paid = this->ammount * this->interest;
+        this->actual_debt_paid = this->payment - this->actual_interest_paid;
+        this->ammount_paid += this->payment;
+        this->ammount -= this->actual_debt_paid;
+
+        // extraordinary_ammounts there
+    }
+
+    map<string, int> month_data() {
+        map<string, int> data;
+
+        data["initial_debt"] = this->initial_debt;
+        data["time"] = this->time;
+        data["interest"] = this->interest;
+        data["payment"] = this->payment;
+        data["time_passed"] = this->time_passed;
+        data["actual_interest_paid"] = this->actual_interest_paid;
+        data["actual_debt_paid"] = this->actual_debt_paid;
+        data["ammount_paid"] = this->ammount_paid;
+        data["ammount"] = this->ammount;
+
+        return data;
+    }
+
+};
+
+int main(){
+    return 0;
+}
