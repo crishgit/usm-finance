65bc7f450e08281a1d16fa8d58b7e9269dceb054
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-06-08 22:06:34 -0400

    implemented table class and add test case

--- finance-software.cpp
+++ finance-software.cpp
@@ -7,6 +7,8 @@
 #include <typeinfo>
 using namespace std;
 
+
+// ------------------ Classes ------------------ //
 class Debt  {
 private:
     // Initial Variables
@@ -26,7 +28,7 @@ private:
 
     // Report Variables
     double total_interest_pay = 0.0;
-    double total_debt_pay = 0.0;
+    double total_pay = 0.0;
     double left_extraordinary_pay = 0.0;
 
     // ------------------ Private Methods ------------------ //
@@ -57,6 +59,7 @@ private:
     }
 
 public:
+    // ------------------ Public Methods ------------------ //
     Debt (double initial_debt, double interest, double months, 
           map<int, double> extraordinary_pays){
 
@@ -76,7 +79,7 @@ public:
         this->current_extraordinary_pay = 0.0;
         this->months_paying = 0.0;
         this->total_interest_pay = 0.0;
-        this->total_debt_pay = 0.0;
+        this->total_pay = 0.0;
         this->left_extraordinary_pay = this->total_extraordinary_pay;
     }
 
@@ -91,11 +94,14 @@ public:
 
         adjustToMaxPossiblePay(this->current_debt_pay);
         payWith(this->current_debt_pay);
-        this->total_debt_pay += this->current_debt_pay;
 
         adjustToMaxPossiblePay(this->current_extraordinary_pay);
         payWith(this->current_extraordinary_pay);
         this->left_extraordinary_pay -= this->current_extraordinary_pay;
+
+        this->total_pay += this->current_debt_pay + 
+                           this->current_extraordinary_pay + 
+                           this->current_interest_pay;
     }
 
     map<string, double> getInitialData(){
@@ -125,15 +131,162 @@ public:
     map<string, double> getReportData(){
         map<string, double> data = {
             {"total_interest_paid", this->total_interest_pay},
-            {"total_debt_paid", this->total_debt_pay},
+            {"total_paid", this->total_pay},
             {"left_extraordinary_pays", this->left_extraordinary_pay},
         };
 
         return data;
     }
 
+    bool haveDebt(){
+        return (this->current_debt > 0) ? true : false;
+    }
+
 };
 
+// ------------------ Functions ------------------ //
+
+class Table {
+private:
+    // vector_rows[row][column] -> string of that column
+    vector< vector<string> > vector_rows = {};
+    vector<int> columns_width = {};
+    int columns_ammount = 0;
+    int columns_separation = 3;
+    char columns_separator = '|';
+    int rows_ammount = 0;
+
+    // ------------------ Private Methods ------------------ // 
+    void calculateColumnsWidth(){
+        this->columns_width = {};
+
+        for(int i = 0; i < this->columns_ammount; i++){
+            int max_width = 0;
+
+            for(int j = 0; j < this->rows_ammount; j++){
+                int current_width = this->vector_rows[j][i].size();
+
+                if(current_width > max_width){
+                    max_width = current_width;
+                }
+            }
+
+            this->columns_width.push_back(max_width);
+        }
+    }
+
+    void printRow(int row_index){
+        // format:
+        // 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+        vector<string> row = this->vector_rows[row_index];
+        for(int column_i = 0; column_i < this->columns_ammount; column_i++){
+            string column = row[column_i];
+            int column_width = this->columns_width[column_i];
+
+            cout << setw(column_width + this->columns_separation) 
+                 << column << this->columns_separator;
+        }
+
+        cout << endl;
+    }
+
+public:
+    Table(int columns_ammount, int columns_separation = 3, 
+          char columns_separator = '|'){
+
+        if(columns_ammount <= 0){
+            throw invalid_argument("Columns ammount must be greater than 0");
+        }
+        if(columns_separation <= 0){
+            throw invalid_argument("Columns separation must be greater than 0");
+        }
+
+        this->columns_ammount = columns_ammount;
+        this->columns_separation = columns_separation;
+        this->columns_separator = columns_separator;
+    }
+
+    void addRow (vector<string> row){
+        if(row.size() != this->columns_ammount){
+            throw invalid_argument("Row size must be equal to columns ammount");
+        }
+
+        this->vector_rows.push_back(row);
+        this->rows_ammount += 1;
+    }
+
+    void printTable(){
+        this->calculateColumnsWidth();
+        for(int row_i = 0; row_i < this->rows_ammount; row_i++){
+            this->printRow(row_i);
+        }
+    }
+
+    vector<int> getColumnWidth(){
+        return this->columns_width;
+    }
+};
+
+string to_string_with_N_decimals(double number, int decimals = 2){
+  stringstream ss;
+  ss << fixed << setprecision(decimals) << number;
+  return ss.str();
+}
+
+int main () {
+    double ptDebt = 20000.0;
+    double ptInterest = 3;
+    int ptMonth = 3;
+    map<int, double> extraordinary_pays = {
+        {2, 15000.0}
+    };
+
+    Debt user_debt(ptDebt, ptInterest, ptMonth, extraordinary_pays);
+    Table table_debt(7);
+    table_debt.addRow({"Mes", "Deuda Pagada", "Interes Pagado", "Deuda", 
+                       "Pago Extraordinario", "Total Pagado Interes", 
+                       "Total Pagado"});
+
+    while(user_debt.haveDebt() ){
+        user_debt.passMonth();
+        map<string, double> current_month_data = user_debt.getCurrentMonthData();
+        map<string, double> report_data = user_debt.getReportData();
+
+        table_debt.addRow(
+            {
+                "Mes "+ to_string_with_N_decimals( current_month_data["months_paying"], 0), 
+                to_string_with_N_decimals( current_month_data["current_debt_paid"] ),
+                to_string_with_N_decimals( current_month_data["current_interest_paid"] ),
+                to_string_with_N_decimals( current_month_data["current_debt"] ),
+                to_string_with_N_decimals( current_month_data["current_extraordinary_pay"] ),
+                to_string_with_N_decimals( report_data["total_interest_paid"] ),
+                to_string_with_N_decimals( report_data["total_paid"] ),
+            }
+        );
+    }
+    
+    table_debt.printTable();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 void printTable(double ptDebt, int ptInterest, int ptMonth, map<int, double> &extraordinary_pays){
 
      // ------------------ table ------------------ // 
@@ -315,3 +468,5 @@ int main(){
     
     return 0;
 }
+
+*/
